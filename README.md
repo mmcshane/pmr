483d624fdf4e2d59f11f2181ac9d9dc86f0621a6
Matt McShane <matt@mattmcshane.com>
2016-02-10 02:20:31 +0000

    Source docs for resource_adapter

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Polymorphic Memory Resources
 
 This is a work in progress standalone implementation of [N3916](http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3916.pdf).
 
-| Class/Function                    | Status    |
-| --------------------------------- | --------- |
-| pmr::memory_resource              | Complete  |
-| pmr::new_delete_resource          | Complete  |
-| pmr::null_memory_resource         | Complete  |
-| pmr::get_default_resource         | Complete  |
-| pmr::set_default_resource         | Complete  |
-| pmr::monotonic_buffer_resource    | Complete  |
-| pmr::polymorphic_allocator        | Complete  |
-| pmr::synchronized_pool_resource   | Absent    |
-| pmr::unsynchronized_pool_resource | Absent    |
-| pmr::resource_adapter             | Complete  |
+| Class/Function                        | Status    |
+| ------------------------------------- | --------- |
+| pmr::memory_resource                  | Complete  |
+| new_delete_resource()                 | Complete  |
+| null_memory_resource()                | Complete  |
+| get_default_resource()                | Complete  |
+| set_default_resource()                | Complete  |
+| pmr::monotonic_buffer_resource        | Complete  |
+| pmr::polymorphic_allocator            | Complete  |
+| pmr::synchronized_pool_resource       | Absent    |
+| pmr::unsynchronized_pool_resource     | Absent    |
+| pmr::resource_adapter                 | Complete  |
--- include/pmr/resource_adapter.hpp
+++ include/pmr/resource_adapter.hpp
@@ -8,7 +8,6 @@ namespace pmr
 {
     namespace detail
     {
-
         template <std::size_t align> struct aligned{};
         template <> struct alignas(2)  aligned<2> { char _[ 2]; };
         template <> struct alignas(4)  aligned<4> { char _[ 4]; };
@@ -16,159 +15,187 @@ namespace pmr
         template <> struct alignas(16) aligned<16>{ char _[16]; };
         template <> struct alignas(32) aligned<32>{ char _[32]; };
         template <> struct alignas(64) aligned<64>{ char _[64]; };
+    }
 
 
-        template <typename Alloc>
-        class resource_adapter_impl : public memory_resource
-        {
-          public:
-            using allocator_type = Alloc;
+    //! Adapts any allocator type to the memory_resource interface
+    //!
+    //! \tparam Alloc The type of allocator to adapt
+    template <typename Alloc>
+    class resource_adapter : public memory_resource
+    {
+      public:
+        using allocator_type = Alloc;
 
-            resource_adapter_impl();
-            resource_adapter_impl(const resource_adapter_impl&) = default;
-            resource_adapter_impl(resource_adapter_impl&&) = default;
-            explicit resource_adapter_impl(const allocator_type& alloc);
-            explicit resource_adapter_impl(allocator_type&& alloc);
+        //! Instantiate and use a default-constructed instance of allocator_type
+        resource_adapter();
 
-            resource_adapter_impl& operator=(
-                    const resource_adapter_impl&) = default;
+        resource_adapter(const resource_adapter&) = default;
+        resource_adapter(resource_adapter&&) = default;
 
-            allocator_type get_allocator() const;
-
-          protected:
-            void* do_allocate(std::size_t bytes, std::size_t align);
-            void do_deallocate(void* ptr, std::size_t bytes, std::size_t align);
-            bool do_is_equal(const memory_resource& other) const;
-
-          private:
-            using traits = std::allocator_traits<allocator_type>;
-            static_assert(
-                std::is_same<typename traits::value_type, char>::value,
-                "resource_adapter_imp only instantiable with Allocator<char>");
-
-            template <std::size_t align>
-            void* do_allocate(std::size_t bytes);
-
-            template <std::size_t align>
-            void do_deallocate(void* ptr, std::size_t bytes);
-
-            Alloc m_alloc;
-        };
+        //! Instantiate specifying an allocator instance
+        //!
+        //! \param alloc an instance of the Allocator concept.
+        explicit resource_adapter(const allocator_type& alloc);
 
 
-        template <typename Alloc>
-        resource_adapter_impl<Alloc>::resource_adapter_impl()
-        {
-        }
+        //! Instantiate specifying an allocator instance
+        //!
+        //! \param alloc an instance of the Allocator concept.
+        explicit resource_adapter(allocator_type&& alloc);
 
+        resource_adapter& operator=(
+                const resource_adapter&) = default;
 
-        template <typename Alloc>
-        resource_adapter_impl<Alloc>::resource_adapter_impl(
-                const allocator_type& alloc)
-            : m_alloc{alloc}
-        {
-        }
+        //! Observe the allocator instance wrapped by this adapter
+        //!
+        //! \returns An instance of allocator_type by-value
+        allocator_type get_allocator() const;
 
+      protected:
+        //! Allocates memory using this instances instance of allocator_type
+        //!
+        //! \param bytes The number of bytes to allocate
+        //! \param align The required alignment of the returned pointer
+        //! \returns A pointer to a suitable aligned block of memory with a size
+        //!          at least bytes
+        void* do_allocate(std::size_t bytes, std::size_t align);
 
-        template <typename Alloc>
-        resource_adapter_impl<Alloc>::resource_adapter_impl(
-                allocator_type&& alloc)
-            : m_alloc{std::move(alloc)}
-        {
-        }
+        //! Deallocates memory previously allocated by this instance
+        //!
+        //! \param ptr A pointer to the memory to deallocate
+        //! \param bytes The number of bytes in the block to deallocatae
+        //! \param align The alignement of the block indicated by ptr
+        void do_deallocate(void* ptr, std::size_t bytes, std::size_t align);
 
+        //! Test that this instance is equal to another memory_resource instance
+        //!
+        //! returns true iff other can by dynamic_cast to resource_adapter and
+        //!         the allocator wrapped by other is equal to (in the
+        //!         operator== sense) the one used by this instance; false
+        //!         otherwise
+        bool do_is_equal(const memory_resource& other) const;
 
-        template <typename Alloc>
-        typename resource_adapter_impl<Alloc>::allocator_type
-        resource_adapter_impl<Alloc>::get_allocator() const
-        {
-            return m_alloc;
-        }
+      private:
+        using traits = std::allocator_traits<allocator_type>;
 
-
-        template <typename Alloc>
-        void*
-        resource_adapter_impl<Alloc>::do_allocate(
-                std::size_t bytes, std::size_t align)
-        {
-            switch(align)
-            {
-                case  2: return do_allocate<2>(bytes);
-                case  4: return do_allocate<4>(bytes);
-                case  8: return do_allocate<8>(bytes);
-                case 16: return do_allocate<16>(bytes);
-                case 32: return do_allocate<32>(bytes);
-                case 64: return do_allocate<64>(bytes);
-                default: throw std::bad_alloc();
-            }
-        }
-
-
-        template <typename Alloc>
-        void
-        resource_adapter_impl<Alloc>::do_deallocate(
-                void* ptr, std::size_t bytes, std::size_t align)
-        {
-            switch(align)
-            {
-                case  2: return do_deallocate<2>(ptr, bytes);
-                case  4: return do_deallocate<4>(ptr, bytes);
-                case  8: return do_deallocate<8>(ptr, bytes);
-                case 16: return do_deallocate<16>(ptr, bytes);
-                case 32: return do_deallocate<32>(ptr, bytes);
-                case 64: return do_deallocate<64>(ptr, bytes);
-                default: throw std::bad_alloc();
-            }
-        }
-
-
-        template <typename Alloc>
-        bool
-        resource_adapter_impl<Alloc>::do_is_equal(
-                const memory_resource& other) const
-        {
-            if(const resource_adapter_impl* p =
-                    dynamic_cast<const resource_adapter_impl*>(&other))
-            {
-                return m_alloc == p->m_alloc;
-            }
-            return false;
-        }
-
-
-        template <typename Alloc>
         template <std::size_t align>
-        void*
-        resource_adapter_impl<Alloc>::do_allocate(std::size_t bytes)
-        {
-            using aligner = aligned<align>;
-            using aligner_traits =
-                typename traits::template rebind_traits<aligner>;
+        void* do_allocate(std::size_t bytes);
 
-            std::size_t n = (bytes + align - 1) / align;
-            typename aligner_traits::allocator_type alloc(m_alloc);
-            return aligner_traits::allocate(alloc, n);
-        }
-
-
-        template <typename Alloc>
         template <std::size_t align>
-        void
-        resource_adapter_impl<Alloc>::do_deallocate(void* ptr, std::size_t bytes)
-        {
-            using aligner = aligned<align>;
-            using aligner_traits =
-                typename traits::template rebind_traits<aligner>;
+        void do_deallocate(void* ptr, std::size_t bytes);
 
-            std::size_t n = (bytes + align - 1) / align;
-            typename aligner_traits::allocator_type alloc(m_alloc);
-            return aligner_traits::deallocate(alloc,
-                    reinterpret_cast<typename aligner_traits::pointer>(ptr), n);
+        Alloc m_alloc;
+    };
+
+
+    template <typename Alloc>
+    resource_adapter<Alloc>::resource_adapter()
+    {
+    }
+
+
+    template <typename Alloc>
+    resource_adapter<Alloc>::resource_adapter(
+            const allocator_type& alloc)
+        : m_alloc{alloc}
+    {
+    }
+
+
+    template <typename Alloc>
+    resource_adapter<Alloc>::resource_adapter(
+            allocator_type&& alloc)
+        : m_alloc{std::move(alloc)}
+    {
+    }
+
+
+    template <typename Alloc>
+    typename resource_adapter<Alloc>::allocator_type
+    resource_adapter<Alloc>::get_allocator() const
+    {
+        return m_alloc;
+    }
+
+
+    template <typename Alloc>
+    void*
+    resource_adapter<Alloc>::do_allocate(
+            std::size_t bytes, std::size_t align)
+    {
+        switch(align)
+        {
+            case  2: return do_allocate<2>(bytes);
+            case  4: return do_allocate<4>(bytes);
+            case  8: return do_allocate<8>(bytes);
+            case 16: return do_allocate<16>(bytes);
+            case 32: return do_allocate<32>(bytes);
+            case 64: return do_allocate<64>(bytes);
+            default: throw std::bad_alloc();
         }
     }
 
 
-    template <typename Allocator>
-    using resource_adapter = detail::resource_adapter_impl<
-        typename std::allocator_traits<Allocator>::template rebind_alloc<char>>;
+    template <typename Alloc>
+    void
+    resource_adapter<Alloc>::do_deallocate(
+            void* ptr, std::size_t bytes, std::size_t align)
+    {
+        switch(align)
+        {
+            case  2: return do_deallocate<2>(ptr, bytes);
+            case  4: return do_deallocate<4>(ptr, bytes);
+            case  8: return do_deallocate<8>(ptr, bytes);
+            case 16: return do_deallocate<16>(ptr, bytes);
+            case 32: return do_deallocate<32>(ptr, bytes);
+            case 64: return do_deallocate<64>(ptr, bytes);
+            default: throw std::bad_alloc();
+        }
+    }
+
+
+    template <typename Alloc>
+    bool
+    resource_adapter<Alloc>::do_is_equal(
+            const memory_resource& other) const
+    {
+        if(const resource_adapter* p =
+                dynamic_cast<const resource_adapter*>(&other))
+        {
+            return m_alloc == p->m_alloc;
+        }
+        return false;
+    }
+
+
+    template <typename Alloc>
+    template <std::size_t align>
+    void*
+    resource_adapter<Alloc>::do_allocate(std::size_t bytes)
+    {
+        using aligner = detail::aligned<align>;
+        using aligner_traits =
+            typename traits::template rebind_traits<aligner>;
+
+        std::size_t n = (bytes + align - 1) / align;
+        typename aligner_traits::allocator_type alloc(m_alloc);
+        return aligner_traits::allocate(alloc, n);
+    }
+
+
+    template <typename Alloc>
+    template <std::size_t align>
+    void
+    resource_adapter<Alloc>::do_deallocate(void* ptr, std::size_t bytes)
+    {
+        using aligner = detail::aligned<align>;
+        using aligner_traits =
+            typename traits::template rebind_traits<aligner>;
+
+        std::size_t n = (bytes + align - 1) / align;
+        typename aligner_traits::allocator_type alloc(m_alloc);
+        return aligner_traits::deallocate(alloc,
+                reinterpret_cast<typename aligner_traits::pointer>(ptr), n);
+    }
 }
